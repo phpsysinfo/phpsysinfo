0c852397c8371c174255a7178404227e1841c44a
Erkan <erkan2005@gmail.com>
2013-02-06 20:35:37 +0100

    reformat

--- includes/mb/class.pitemp.inc.php
+++ includes/mb/class.pitemp.inc.php
@@ -15,10 +15,10 @@ class PiTemp extends Sensors
 {
     private function _temperature()
     {
-	$temp = file_get_contents('/sys/class/thermal/thermal_zone0/temp');
-	$temp_max = file_get_contents('/sys/class/thermal/thermal_zone0/trip_point_0_temp');
-	$temp = $temp /1000;
-	$temp_max = $temp_max/1000;
+        $temp = file_get_contents('/sys/class/thermal/thermal_zone0/temp');
+        $temp_max = file_get_contents('/sys/class/thermal/thermal_zone0/trip_point_0_temp');
+        $temp = $temp /1000;
+        $temp_max = $temp_max/1000;
         $dev = new SensorDevice();
         $dev->setName("CPU ".(1));
         $dev->setValue($temp);
