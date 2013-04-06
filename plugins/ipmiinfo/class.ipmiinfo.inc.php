68369fd1473cff8452310a5816d4d214a4e0d01e
namiltd <namiltd@users.sf.net>
2013-04-06 11:15:44 +0200

    fix parenthesis error

--- plugins/ipmiinfo/class.ipmiinfo.inc.php
+++ plugins/ipmiinfo/class.ipmiinfo.inc.php
@@ -136,7 +136,7 @@ class ipmiinfo extends PSI_Plugin
                 $this->_lines = preg_split("/\n/", $lines, -1, PREG_SPLIT_NO_EMPTY);
                 break;
             case 'data':
-                if (CommonFunctions::rfts(APP_ROOT."/data/ipmiinfo.txt", $lines) && !empty($lines)))
+                if (CommonFunctions::rfts(APP_ROOT."/data/ipmiinfo.txt", $lines) && !empty($lines))
                 $this->_lines = preg_split("/\n/", $lines, -1, PREG_SPLIT_NO_EMPTY);
                 break;
             default:
