8a2bb7eea27826fc74b03ce66a0faf6a54d423bf
rk4an <erkan2005@gmail.com>
2013-09-22 10:16:22 +0200

    phpSysInfo 3.1.7

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ if (!defined('PSI_CONFIG_FILE')) {
     /**
      * phpSysInfo version
      */
-    define('PSI_VERSION','3.1.6');
+    define('PSI_VERSION','3.1.7');
     /**
      * phpSysInfo configuration
      */
