ac15b51b2e6d931b9826e8b3fcb52434e7b154a0
namiltd <namiltd@users.sf.net>
2014-02-22 22:38:48 +0100

    avoid PRETTY_NAME defined by variable

--- includes/os/class.Linux.inc.php
+++ includes/os/class.Linux.inc.php
@@ -881,7 +881,8 @@ class Linux extends OS
                         }
                         $this->sys->setDistributionIcon($list['Tails']['Image']);
                     } else {
-                        if (preg_match('/^PRETTY_NAME="?([^"\n]+)"?/m', $buf, $desc_buf)) {
+                        if (preg_match('/^PRETTY_NAME="?([^"\n]+)"?/m', $buf, $desc_buf)
+                           && !preg_match('/\$/', $desc_buf[1])) { //if is not defined by variable
                             $this->sys->setDistribution(trim($desc_buf[1]));
                         } else {
                             if (isset($list[trim($id_buf[1])]['Name'])) {
