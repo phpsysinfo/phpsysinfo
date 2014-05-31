427d8ea46ccbc87ff09b32d7efe3621f85eddb12
rk4an <erkan2005@gmail.com>
2014-05-31 13:02:12 +0200

    3.1.13 released

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ if (!defined('PSI_CONFIG_FILE')) {
     /**
      * phpSysInfo version
      */
-    define('PSI_VERSION','3.1.12');
+    define('PSI_VERSION','3.1.13');
     /**
      * phpSysInfo configuration
      */
