66d493a38d132b9327fcc95ef773c460e484ece4
namiltd <namiltd@users.sf.net>
2014-06-23 07:02:59 +0200

    tab to space

--- includes/mb/class.ipmiutil.inc.php
+++ includes/mb/class.ipmiutil.inc.php
@@ -41,11 +41,11 @@ class IPMIutil extends Sensors
         switch (strtolower(PSI_SENSOR_ACCESS)) {
         case 'command':
             CommonFunctions::executeProgram('ipmiutil', 'sensor -st', $lines);
-            $this->_lines = preg_split("/\r?\n/", preg_replace("/\r?\n\t\|/", "\t|",$lines), -1, PREG_SPLIT_NO_EMPTY);
+            $this->_lines = preg_split("/\r?\n/", preg_replace("/\r?\n\t\|/", " |",$lines), -1, PREG_SPLIT_NO_EMPTY);
             break;
         case 'file':
             if (CommonFunctions::rfts(APP_ROOT.'/data/ipmiutil.txt', $lines)) {
-                $this->_lines = preg_split("/\r?\n/", preg_replace("/\r?\n\t\|/", "\t|",$lines), -1, PREG_SPLIT_NO_EMPTY);
+                $this->_lines = preg_split("/\r?\n/", preg_replace("/\r?\n\t\|/", " |",$lines), -1, PREG_SPLIT_NO_EMPTY);
             }
             break;
         default:
