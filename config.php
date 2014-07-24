237aab4a90b6580e2e10d5fb78368fd2435fe7ee
namiltd <namiltd@users.sf.net>
2014-07-25 00:22:35 +0200

    3.1.x

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ if (!defined('PSI_CONFIG_FILE')) {
     /**
      * phpSysInfo version
      */
-    define('PSI_VERSION', '3.1.14');
+    define('PSI_VERSION', '3.1.x');
     /**
      * phpSysInfo configuration
      */
