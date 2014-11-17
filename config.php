ba6ae2d27c7a626aebc9cebdd804732d0c63acb5
namiltd <namiltd@users.sf.net>
2014-11-17 20:00:04 +0100

    Release 3.1.17

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ if (!defined('PSI_CONFIG_FILE')) {
     /**
      * phpSysInfo version
      */
-    define('PSI_VERSION', '3.1.16');
+    define('PSI_VERSION', '3.1.17');
     /**
      * phpSysInfo configuration
      */
