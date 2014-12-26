24a015fc4625c9e7d45547aef7bed560e7ff527d
namiltd <namiltd@poczta.onet.pl>
2014-12-26 21:04:06 +0100

    no updates info

--- plugins/updatenotifier/js/updatenotifier_bootstrap.js
+++ plugins/updatenotifier/js/updatenotifier_bootstrap.js
@@ -14,6 +14,10 @@ function renderPlugin_UpdateNotifier(data) {
     };
     if ((data['Plugins']['Plugin_UpdateNotifier'] !== undefined) && (data['Plugins']['Plugin_UpdateNotifier']["UpdateNotifier"] !== undefined)){
         $('#updatenotifier').render(data['Plugins']['Plugin_UpdateNotifier']["UpdateNotifier"], directives);
+        if ((data['Plugins']['Plugin_UpdateNotifier']["UpdateNotifier"]["packages"] == 0) &&
+            (data['Plugins']['Plugin_UpdateNotifier']["UpdateNotifier"]["security"] == 0) ) {
+            $("#updatenotifier-info").html("<strong>No updates available</strong>");
+        }
         $('#block_updatenotifier').show();
     }
 }
