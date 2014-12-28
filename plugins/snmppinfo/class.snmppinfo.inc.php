deab53de04af9d3b4a8c67c4b7f433776bd011aa
namiltd <namiltd@poczta.onet.pl>
2014-12-28 19:33:09 +0100

    snmppinfo CRLF - LF insensibility

--- plugins/snmppinfo/class.snmppinfo.inc.php
+++ plugins/snmppinfo/class.snmppinfo.inc.php
@@ -170,7 +170,7 @@ class SNMPPInfo extends PSI_Plugin
             return;
         }
         foreach ($this->_filecontent as $printer=>$result) {
-            $lines = preg_split('/\n/', $result);
+            $lines = preg_split('/\r?\n/', $result);
             foreach ($lines as $line) {
                 if (preg_match('/^\.1\.3\.6\.1\.2\.1\.43\.11\.1\.1\.6\.1\.(.*) = STRING:\s(.*)/', $line, $data)) {
                     $this->_result[$printer][$data[1]]['prtMarkerSuppliesDescription']=trim($data[2],"\"");
