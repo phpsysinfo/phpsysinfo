52a75c0d641a35699050829d3732969a36a7d86f
namiltd <namiltd@poczta.onet.pl>
2014-12-29 23:46:46 +0100

    code optimization

--- plugins/uprecords/js/uprecords_bootstrap.js
+++ plugins/uprecords/js/uprecords_bootstrap.js
@@ -11,7 +11,6 @@ function renderPlugin_uprecords(data) {
     if ((data['Plugins']['Plugin_uprecords'] !== undefined) && (data['Plugins']['Plugin_uprecords']['Uprecords'] !== undefined)) {
         var upitems = items(data['Plugins']['Plugin_uprecords']['Uprecords']['Item']);
         if (upitems.length > 0) {
-        $('#block_uprecords').show();
             var up_memory = [];
             for (i = 0; i < upitems.length ; i++) {
                 up_memory.push(upitems[i]["@attributes"]);
