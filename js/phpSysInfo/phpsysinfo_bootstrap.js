ec3701ade3e0f087078ca8ab0ab762f5a2bfa0af
namiltd <namiltd@users.noreply.github.com>
2015-05-10 19:20:13 +0200

    icon width 32px

--- js/phpSysInfo/phpsysinfo_bootstrap.js
+++ js/phpSysInfo/phpsysinfo_bootstrap.js
@@ -108,7 +108,7 @@ function renderVitals(data) {
         },
         Distro: {
             html: function () {
-                return '<img src="gfx/images/' + this["Distroicon"] + '" style="width:20px"/>' + " " +this["Distro"];
+                return '<img src="gfx/images/' + this["Distroicon"] + '" style="width:32px"/>' + " " +this["Distro"];
             }
         },
         LoadAvg: {
