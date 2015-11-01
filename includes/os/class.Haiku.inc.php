ff163dfd1e555f0f558db568fe6e645a7a28fd8e
namiltd <namiltd@users.noreply.github.com>
2015-11-01 11:57:16 +0100

    incorrect backet

--- includes/os/class.Haiku.inc.php
+++ includes/os/class.Haiku.inc.php
@@ -340,7 +340,7 @@ class Haiku extends OS
                                     $dev->setInfo(preg_replace('/:/', '-', $ar_buf2[1]));
                             elseif (preg_match('/^\s+inet\saddr:\s(\S*),/i', $line, $ar_buf2))
                                      $dev->setInfo(($dev->getInfo()?$dev->getInfo().';':'').$ar_buf2[1]);
-                                 elseif (preg_match('/^\s+inet6\saddr:\s(\S*),/i', $line, $ar_buf2))
+                                 elseif (preg_match('/^\s+inet6\saddr:\s(\S*),/i', $line, $ar_buf2)
                                           && ($ar_buf2[1]!="::") && !preg_match('/^fe80::/i', $ar_buf2[1]))
                                             $dev->setInfo(($dev->getInfo()?$dev->getInfo().';':'').$ar_buf2[1]);
                         }
