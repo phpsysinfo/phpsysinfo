3a2e5fd9150aa6b84f1df653c56cb39d720e2110
namiltd <namiltd@users.noreply.github.com>
2015-12-13 18:40:28 +0100

    Release 3.2.4

--- read_config.php
+++ read_config.php
@@ -3,7 +3,7 @@ if (!defined('PSI_CONFIG_FILE')) {
     /**
      * phpSysInfo version
      */
-    define('PSI_VERSION', '3.2.3');
+    define('PSI_VERSION', '3.2.4');
     /**
      * phpSysInfo configuration
      */
