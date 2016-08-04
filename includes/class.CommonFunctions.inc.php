19d3859b59a1001debacf558f4260a972ced148f
namiltd <namiltd@users.noreply.github.com>
2016-08-04 22:34:27 +0200

    fpen error handling

--- includes/class.CommonFunctions.inc.php
+++ includes/class.CommonFunctions.inc.php
@@ -295,7 +295,20 @@ class CommonFunctions
         $error = PSI_Error::singleton();
         if (file_exists($strFileName)) {
             if (is_readable($strFileName)) {
+                /* default error handler */
+                if (function_exists('errorHandlerPsi')) {
+                    restore_error_handler();
+                }
+                /* fatal errors only */
+                $old_err_rep = error_reporting();
+                error_reporting(E_ERROR);
                 if ($fd = fopen($strFileName, 'r')) {
+                    /* restore error level */
+                    error_reporting($old_err_rep);
+                    /* restore error handler */
+                    if (function_exists('errorHandlerPsi')) {
+                        set_error_handler('errorHandlerPsi');
+                    }
                     while (!feof($fd)) {
                         $strFile .= fgets($fd, $intBytes);
                         if ($intLines <= $intCurLine && $intLines != 0) {
@@ -321,6 +334,12 @@ class CommonFunctions
                     return false;
                 }
             } else {
+                /* restore error level */
+                error_reporting($old_err_rep);
+                /* restore error handler */
+                if (function_exists('errorHandlerPsi')) {
+                    set_error_handler('errorHandlerPsi');
+                }
                 if ($booErrorRep) {
                     $error->addError('fopen('.$strFileName.')', 'file permission error');
                 }
