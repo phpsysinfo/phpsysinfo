3c4768d4d839cb9955e1fe468d75e9e87dd1679f
namiltd <namiltd@users.noreply.github.com>
2016-10-07 20:47:47 +0200

    Tunnel adapter

--- includes/os/class.WINNT.inc.php
+++ includes/os/class.WINNT.inc.php
@@ -496,7 +496,7 @@ class WINNT extends OS
         if ($this->_wmi) {
             $allDevices = CommonFunctions::getWMI($this->_wmi, 'Win32_PerfRawData_Tcpip_NetworkInterface', array('Name', 'BytesSentPersec', 'BytesTotalPersec', 'BytesReceivedPersec', 'PacketsReceivedErrors', 'PacketsReceivedDiscarded', 'CurrentBandwidth'));
             if (!$allDevices && CommonFunctions::executeProgram('ipconfig', '/all', $devicesbuf, false) && (trim($devicesbuf) !== "")) {
-                $netdevices = preg_split('/^(Ethernet)|(Wireless) [^\n]+\n\r\n/m', $devicesbuf, -1, PREG_SPLIT_NO_EMPTY);
+                $netdevices = preg_split('/^(Ethernet)|(Wireless)|(Tunnel) [^\n]+\n\r\n/m', $devicesbuf, -1, PREG_SPLIT_NO_EMPTY);
                 if (sizeof($netdevices)>1)  foreach ($netdevices as $devnr=>$netdevice) if ($devnr > 0) {
                     $bufe = preg_split("/\r\n   /", trim($netdevice), -1, PREG_SPLIT_NO_EMPTY);
                     $notdiss = true;
