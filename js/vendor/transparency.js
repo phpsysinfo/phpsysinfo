2d92dd04829e12628b784381bcdcc1dcc32455a7
namiltd <namiltd@users.noreply.github.com>
2016-11-22 09:51:04 +0100

    License info

--- js/vendor/transparency.js
+++ js/vendor/transparency.js
@@ -1,3 +1,8 @@
+/* ========================================================================
+ * transparency.js v0.1.0
+ * http://leonidas.github.io/transparency/
+ * Licensed under MIT (https://github.com/leonidas/transparency/blob/master/LICENSE)
+ * ======================================================================== */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var $, Context, Transparency, helpers, _,
   __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
