a5167d776245f7fdec7df960fe41c8b9d2a5a72b
namiltd <namiltd@users.noreply.github.com>
2016-12-03 11:12:00 +0100

    removing cputest

--- tools/cputest.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: text/plain");
-$filename = '/proc/cpuinfo';
-echo "----------".$filename."----------\n";
-echo $contents=file_get_contents($filename);
-if ((strlen($contents)>0)&&(substr($contents, -1)!="\n")) {
-    echo "\n";
-}
