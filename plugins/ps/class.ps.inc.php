ee624747ec46ee73c073f5d1cd7295721da9f580
namiltd <namiltd@users.noreply.github.com>
2017-06-30 21:03:41 +0200

    PS_SHOW_PID1CHILD_EXPANDED implementation

--- plugins/ps/class.ps.inc.php
+++ plugins/ps/class.ps.inc.php
@@ -252,11 +252,9 @@ class PS extends PSI_Plugin
                 $xmlnode->addAttribute('MemoryUsage', $value[2]);
                 $xmlnode->addAttribute('CPUUsage', $value[3]);
                 $xmlnode->addAttribute('Name', $value[4]);
-                if (PSI_OS !== 'WINNT') {
-                    if (($parentid === 1)
-                       || (defined('PSI_PLUGIN_PS_SHOW_KTHREADD_EXPANDED') && (PSI_PLUGIN_PS_SHOW_KTHREADD_EXPANDED === false) && ($value[3] === "[kthreadd]"))) {
-                        $xmlnode->addAttribute('Expanded', 0);
-                    }
+                if ((PSI_OS !== 'WINNT') &&
+                    ((($parentid === 1) && (!defined('PSI_PLUGIN_PS_SHOW_PID1CHILD_EXPANDED') || (PSI_PLUGIN_PS_SHOW_PID1CHILD_EXPANDED === false)))
+                    || (defined('PSI_PLUGIN_PS_SHOW_KTHREADD_EXPANDED') && (PSI_PLUGIN_PS_SHOW_KTHREADD_EXPANDED === false) && ($value[4] === "[kthreadd]")))) {
                 }
             }
             if (isset($value['childs'])) {
