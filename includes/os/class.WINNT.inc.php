65fb309c413e6e761fa95196946e6fbc45588c5c
namiltd <namiltd@users.sf.net>
2017-11-13 21:27:27 +0100

    Manufacturer 'Not Specified'

--- includes/os/class.WINNT.inc.php
+++ includes/os/class.WINNT.inc.php
@@ -527,7 +527,7 @@ class WINNT extends OS
         $buffer = $this->_get_Win32_ComputerSystem(); 
         if ($buffer) {
             $buf = "";
-            if (isset($buffer[0]['Manufacturer']) && !preg_match(/"^To be filled by O\.E\.M\.$|^System manufacturer$/i", $buf2=$buffer[0]['Manufacturer'])) {
+            if (isset($buffer[0]['Manufacturer']) && !preg_match(/"^To be filled by O\.E\.M\.$|^System manufacturer$|^Not Specified$/i", $buf2=$buffer[0]['Manufacturer'])) {
                 $buf .= ' '.$buf2;
             }
             
