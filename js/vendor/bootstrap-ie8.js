1fad5b8d05832db0cb9704131008aa8379503101
namiltd <namiltd@users.sf.net>
2018-04-24 18:43:41 +0200

    cosmetic code change

--- js/vendor/bootstrap-ie8.js
+++ js/vendor/bootstrap-ie8.js
@@ -1,7 +1,6 @@
 // create the nodeType constants if the Node object is not defined
 if (!window.Node){
-  var Node =
-      {
+    var Node = {
         ELEMENT_NODE                :  1,
         ATTRIBUTE_NODE              :  2,
         TEXT_NODE                   :  3,
@@ -14,61 +13,62 @@ if (!window.Node){
         DOCUMENT_TYPE_NODE          : 10,
         DOCUMENT_FRAGMENT_NODE      : 11,
         NOTATION_NODE               : 12
-      };
+    };
 }
 
 (function() {
-  if (!Object.keys) {
-    Object.keys = function(obj) {
-      if (obj !== Object(obj)) {
-        throw new TypeError('Object.keys called on a non-object');
-      }
+    if (!Object.keys) {
+        Object.keys = function(obj) {
+            if (obj !== Object(obj)) {
+                throw new TypeError('Object.keys called on a non-object');
+            }
 
-      var keys = [];
+            var keys = [];
 
-      for (var i in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, i)) {
-          keys.push(i);
-        }
-      }
+            for (var i in obj) {
+                if (Object.prototype.hasOwnProperty.call(obj, i)) {
+                    keys.push(i);
+                }
+            }
 
-      return keys;
-    };
-  }
+            return keys;
+        };
+    }
 }());
 
 
 (function() {
-  if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function(fn, scope) {
-        for(var i = 0, len = this.length; i < len; ++i) {
-            fn.call(scope, this[i], i, this);
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function(fn, scope) {
+            for(var i = 0, len = this.length; i < len; ++i) {
+                fn.call(scope, this[i], i, this);
+            }
         }
     }
-  }
 }());
 
 // ES 15.2.3.6 Object.defineProperty ( O, P, Attributes )
 // Partial support for most common case - getters, setters, and values
 (function() {
-  if (!Object.defineProperty ||
-      !(function () { try { Object.defineProperty({}, 'x', {}); return true; } catch (e) { return false; } } ())) {
-    var orig = Object.defineProperty;
-    Object.defineProperty = function (o, prop, desc) {
-      // In IE8 try built-in implementation for defining properties on DOM prototypes.
-      if (orig) { try { return orig(o, prop, desc); } catch (e) {} }
+    if (!Object.defineProperty ||
+       !(function () { try { Object.defineProperty({}, 'x', {}); return true; } catch (e) { return false; } } ())) {
+        var orig = Object.defineProperty;
+        Object.defineProperty = function (o, prop, desc) {
+            // In IE8 try built-in implementation for defining properties on DOM prototypes.
+            if (orig) { try { return orig(o, prop, desc); } catch (e) {} }
 
-      if (o !== Object(o)) { throw TypeError("Object.defineProperty called on non-object"); }
-      if (Object.prototype.__defineGetter__ && ('get' in desc)) {
-        Object.prototype.__defineGetter__.call(o, prop, desc.get);
-      }
-      if (Object.prototype.__defineSetter__ && ('set' in desc)) {
-        Object.prototype.__defineSetter__.call(o, prop, desc.set);
-      }
-      if ('value' in desc) {
-        o[prop] = desc.value;
-      }
-      return o;
-    };
-  }
+            if (o !== Object(o)) { throw TypeError("Object.defineProperty called on non-object"); }
+            if (Object.prototype.__defineGetter__ && ('get' in desc)) {
+                Object.prototype.__defineGetter__.call(o, prop, desc.get);
+            }
+            if (Object.prototype.__defineSetter__ && ('set' in desc)) {
+                Object.prototype.__defineSetter__.call(o, prop, desc.set);
+            }
+            if ('value' in desc) {
+                o[prop] = desc.value;
+            }
+
+            return o;
+        };
+    }
 }());
