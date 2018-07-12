da10e6e5ac3d596df31d38d6b3d2b4a261013c03
namiltd <namiltd@users.sf.net>
2018-07-12 22:03:55 +0200

    bootstrap 4.1.2

--- js/vendor/bootstrap-ie8.js
+++ js/vendor/bootstrap-ie8.js
@@ -1,4 +1,4 @@
-/* Bootstrap 4 for IE8 - v4.1.1            */
+/* Bootstrap 4 for IE8 - v4.1.2            */
 /* https://github.com/coliff/bootstrap-ie8 */
 
 // create the nodeType constants if the Node object is not defined
@@ -132,6 +132,74 @@ if (!window.Node){
         };
     }
 }());
+
+(function() {
+  var _slice = Array.prototype.slice;
+  Array.prototype.slice = function() {
+    if(this instanceof Array) {
+      return _slice.apply(this, arguments);
+    } else {
+      var result = [];
+      var start = (arguments.length >= 1) ? arguments[0] : 0;
+      var end = (arguments.length >= 2) ? arguments[1] : this.length;
+      for(var i=start; i<end; i++) {
+        result.push(this[i]);
+      }
+      return result;
+    }
+  };
+})();
+
+// adds classList support (as Array) to Element.prototype for IE8-9
+(function() {
+  Object.defineProperty(Element.prototype, 'classList', {
+    get:function(){
+        var element=this,domTokenList=(element.getAttribute('class')||'').replace(/^\s+|\s$/g,'').split(/\s+/g);
+        if (domTokenList[0]==='') domTokenList.splice(0,1);
+        function setClass(){
+            if (domTokenList.length > 0) element.setAttribute('class', domTokenList.join(' '));
+            else element.removeAttribute('class');
+        }
+        domTokenList.toggle=function(className,force){
+            if (force!==undefined){
+                if (force) domTokenList.add(className);
+                else domTokenList.remove(className);
+            }
+            else {
+                if (domTokenList.indexOf(className)!==-1) domTokenList.splice(domTokenList.indexOf(className),1);
+                else domTokenList.push(className);
+            }
+            setClass();
+        };
+        domTokenList.add=function(){
+            var args=[].slice.call(arguments)
+            for (var i=0,l=args.length;i<l;i++){
+                if (domTokenList.indexOf(args[i])===-1) domTokenList.push(args[i])
+            };
+            setClass();
+        };
+        domTokenList.remove=function(){
+            var args=[].slice.call(arguments)
+            for (var i=0,l=args.length;i<l;i++){
+                if (domTokenList.indexOf(args[i])!==-1) domTokenList.splice(domTokenList.indexOf(args[i]),1);
+            };
+            setClass();
+        };
+        domTokenList.item=function(i){
+            return domTokenList[i];
+        };
+        domTokenList.contains=function(className){
+            return domTokenList.indexOf(className)!==-1;
+        };
+        domTokenList.replace=function(oldClass,newClass){
+            if (domTokenList.indexOf(oldClass)!==-1) domTokenList.splice(domTokenList.indexOf(oldClass),1,newClass);
+            setClass();
+        };
+        domTokenList.value = (element.getAttribute('class')||'');
+        return domTokenList;
+    }
+  })
+})();
 
 /**
  * Modified code based on remPolyfill.js (c) Nicolas Bouvrette https://github.com/nbouvrette/remPolyfill
--- js/vendor/bootstrap-ie9.js
+++ js/vendor/bootstrap-ie9.js
@@ -1,4 +1,4 @@
-/* Bootstrap 4 for IE9 - v4.1.1            */
+/* Bootstrap 4 for IE9 - v4.1.2            */
 /* https://github.com/coliff/bootstrap-ie8 */
 
 /**
@@ -11,6 +11,57 @@
  * 2) Added REM support.
  *
  **/
+
+// adds classList support (as Array) to Element.prototype for IE8-9
+(function() {
+  Object.defineProperty(Element.prototype, 'classList', {
+    get:function(){
+        var element=this,domTokenList=(element.getAttribute('class')||'').replace(/^\s+|\s$/g,'').split(/\s+/g);
+        if (domTokenList[0]==='') domTokenList.splice(0,1);
+        function setClass(){
+            if (domTokenList.length > 0) element.setAttribute('class', domTokenList.join(' '));
+            else element.removeAttribute('class');
+        }
+        domTokenList.toggle=function(className,force){
+            if (force!==undefined){
+                if (force) domTokenList.add(className);
+                else domTokenList.remove(className);
+            }
+            else {
+                if (domTokenList.indexOf(className)!==-1) domTokenList.splice(domTokenList.indexOf(className),1);
+                else domTokenList.push(className);
+            }
+            setClass();
+        };
+        domTokenList.add=function(){
+            var args=[].slice.call(arguments)
+            for (var i=0,l=args.length;i<l;i++){
+                if (domTokenList.indexOf(args[i])===-1) domTokenList.push(args[i])
+            };
+            setClass();
+        };
+        domTokenList.remove=function(){
+            var args=[].slice.call(arguments)
+            for (var i=0,l=args.length;i<l;i++){
+                if (domTokenList.indexOf(args[i])!==-1) domTokenList.splice(domTokenList.indexOf(args[i]),1);
+            };
+            setClass();
+        };
+        domTokenList.item=function(i){
+            return domTokenList[i];
+        };
+        domTokenList.contains=function(className){
+            return domTokenList.indexOf(className)!==-1;
+        };
+        domTokenList.replace=function(oldClass,newClass){
+            if (domTokenList.indexOf(oldClass)!==-1) domTokenList.splice(domTokenList.indexOf(oldClass),1,newClass);
+            setClass();
+        };
+        domTokenList.value = (element.getAttribute('class')||'');
+        return domTokenList;
+    }
+  })
+})();
 
 /**
  * For browsers that do not support REM units, fallback to pixels.
