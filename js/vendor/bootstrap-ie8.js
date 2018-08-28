d21acf3336d5564fea37d0c4f233eebfec9e92ec
namiltd <namiltd@users.noreply.github.com>
2018-08-29 00:39:29 +0200

    bootstrap-ie v4.1.3-4

--- js/vendor/bootstrap-ie8.js
+++ js/vendor/bootstrap-ie8.js
@@ -1,4 +1,4 @@
-/* Bootstrap 4 for IE8 - v4.1.304          */
+/* Bootstrap 4 for IE8 - v4.1.3-x          */
 /* https://github.com/namiltd/bootstrap-ie */
 
 // create the nodeType constants if the Node object is not defined
