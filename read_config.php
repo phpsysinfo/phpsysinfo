55bb0b036c2a8b092808318b173999e6791e60f8
namiltd <namiltd@users.noreply.github.com>
2019-09-28 15:15:30 +0200

    not parsable error

--- read_config.php
+++ read_config.php
@@ -11,8 +11,11 @@ if (!defined('PSI_CONFIG_FILE')) {
 
     define('ARRAY_EXP', '/^return array \([^;]*\);$/'); //array expression search
 
-    if (!is_readable(PSI_CONFIG_FILE) || !($config = @parse_ini_file(PSI_CONFIG_FILE, true))) {
+    if (!is_readable(PSI_CONFIG_FILE)) {
         echo "ERROR: phpsysinfo.ini does not exist or is not readable by the webserver in the phpsysinfo directory";
+        die();
+    } elseif  (!($config = @parse_ini_file(PSI_CONFIG_FILE, true))) {
+        echo "ERROR: phpsysinfo.ini file is not parsable";
         die();
     } else {
         foreach ($config as $name=>$group) {
