2ecbb892a936b54e2e6b6d5ebf259c04b34de9bf
namiltd <namiltd@users.noreply.github.com>
2020-01-11 16:31:50 +0100

    v3.3.2

--- read_config.php
+++ read_config.php
@@ -3,7 +3,7 @@ if (!defined('PSI_CONFIG_FILE')) {
     /**
      * phpSysInfo version
      */
-    define('PSI_VERSION', '3.3.1');
+    define('PSI_VERSION', '3.3.2');
     /**
      * phpSysInfo configuration
      */
