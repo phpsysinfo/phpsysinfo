a194de49ec94f8aba5badd8fe1d937b4d4e9dbf0
namiltd <namiltd@users.noreply.github.com>
2020-06-07 20:41:09 +0200

    "Default string" remove

--- includes/os/class.Linux.inc.php
+++ includes/os/class.Linux.inc.php
@@ -75,7 +75,7 @@ class Linux extends OS
         }
 
         if ($machine != "") {
-            $machine = trim(preg_replace("/^\/,?/", "", preg_replace("/ ?(To be filled by O\.E\.M\.|System manufacturer|System Product Name|Not Specified) ?/i", "", $machine)));
+            $machine = trim(preg_replace("/^\/,?/", "", preg_replace("/ ?(To be filled by O\.E\.M\.|System manufacturer|System Product Name|Not Specified|Default string) ?/i", "", $machine)));
         }
 
         if (CommonFunctions::fileexists($filename="/etc/config/uLinux.conf") // QNAP detection
