c5fd72fcad9226d239098893f7f95fa632142671
namiltd <namiltd@users.noreply.github.com>
2020-09-25 21:38:37 +0200

    code optimization

--- includes/plugin/class.PSI_Plugin.inc.php
+++ includes/plugin/class.PSI_Plugin.inc.php
@@ -130,7 +130,7 @@ abstract class PSI_Plugin implements PSI_Interface_Plugin
         $root = $dom->createElement("Plugin_".$this->_plugin_name);
         $dom->appendChild($root);
         $this->xml = new SimpleXMLExtended(simplexml_import_dom($dom), $enc);
-        if ((PSI_OS == 'WINNT') && defined('PSI_PLUGIN_'.strtoupper($this->_plugin_name).'_WMI_HOSTNAME') &&
+        if (((PHP_OS == 'WINNT') || (PHP_OS == 'Linux')) && defined('PSI_PLUGIN_'.strtoupper($this->_plugin_name).'_WMI_HOSTNAME') &&
            (!defined('PSI_WMI_HOSTNAME') || (PSI_WMI_HOSTNAME != constant('PSI_PLUGIN_'.strtoupper($this->_plugin_name).'_WMI_HOSTNAME')))) {
             $this->xml->addAttribute('Hostname', constant('PSI_PLUGIN_'.strtoupper($this->_plugin_name).'_WMI_HOSTNAME'));
         }
