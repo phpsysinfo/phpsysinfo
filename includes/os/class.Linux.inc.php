6d758c448a56e3922b74d2a6bc65bd8fd78cd3a3
namiltd <namiltd@users.noreply.github.com>
2021-04-11 19:27:47 +0200

    KDE neon to detected distros

--- includes/os/class.Linux.inc.php
+++ includes/os/class.Linux.inc.php
@@ -1342,7 +1342,7 @@ class Linux extends OS
             return;
         }
         // We have the '2>/dev/null' because Ubuntu gives an error on this command which causes the distro to be unknown
-        if (CommonFunctions::executeProgram('lsb_release', '-a 2>/dev/null', $distro_info, PSI_DEBUG) && (strlen($distro_info) > 0) && !preg_match("/^Distributor ID:\s*Neon$/m", $distro_info)) {
+        if (CommonFunctions::executeProgram('lsb_release', '-a 2>/dev/null', $distro_info, PSI_DEBUG) && strlen($distro_info) > 0) {
             $distro_tmp = preg_split("/\n/", $distro_info, -1, PREG_SPLIT_NO_EMPTY);
             foreach ($distro_tmp as $info) {
                 $info_tmp = preg_split('/:/', $info, 2);
@@ -1360,6 +1360,10 @@ class Linux extends OS
                     }
                 }
             } else {
+                if (isset($distro['Description']) && ($distro['Description'] != "n/a") && isset($distro['Distributor ID']) && $distro['Distributor ID']=="Neon") { // Neon systems
+                    $distro_tmp = preg_split("/\s/", $distro['Description'], -1, PREG_SPLIT_NO_EMPTY);
+                    $distro['Distributor ID'] = $distro_tmp[0];
+                }
                 if (isset($distro['Description'])
                    && preg_match('/^NAME=\s*"?([^"\n]+)"?\s*$/', $distro['Description'], $name_tmp)) {
                    $distro['Description'] = $name_tmp[1];
