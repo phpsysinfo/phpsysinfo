d34bdb5d1dfaea7f87bd84e712725df6091061e2
namiltd <namiltd@users.noreply.github.com>
2021-08-29 13:44:08 +0200

    compatibility with new versions

--- includes/os/class.DragonFly.inc.php
+++ includes/os/class.DragonFly.inc.php
@@ -32,7 +32,7 @@ class DragonFly extends BSDCommon
     public function __construct($blockname = false)
     {
         parent::__construct($blockname);
-        $this->setCPURegExp1("/^cpu(.*)\, (.*) MHz/");
+        $this->setCPURegExp1("/^cpu(.*)\, (.*) MHz|^CPU: (.*) \((.*)-MHz (.*)\)/");
         $this->setCPURegExp2("/^(.*) at scsibus.*: <(.*)> .*/");
         $this->setSCSIRegExp2("/^(da[0-9]+): (.*)MB /");
         $this->setPCIRegExp1("/(.*): <(.*)>(.*) (pci|legacypci)[0-9]+$/");
