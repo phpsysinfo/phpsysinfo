8bdbbde39cd1742b537e4620ef916bfad9525816
David Goodwin <david@codepoets.co.uk>
2021-09-23 19:57:30 +0100

    simplify code (remove some nesting), remove unnecessary \n in header()

--- xml.php
+++ xml.php
@@ -14,42 +14,37 @@ header('Access-Control-Allow-Origin: *');
  * @link      http://phpsysinfo.sourceforge.net
  */
 
- /**
+/**
  * application root path
  *
  * @var string
  */
 define('PSI_APP_ROOT', dirname(__FILE__));
 
-require_once PSI_APP_ROOT.'/includes/autoloader.inc.php';
+require_once PSI_APP_ROOT . '/includes/autoloader.inc.php';
 
 if ((isset($_GET['json']) || isset($_GET['jsonp'])) && !extension_loaded("json")) {
     echo '<Error Message="The json extension to php required!" Function="ERROR"/>';
 } else {
     // check what xml part should be generated
-    if (isset($_GET['plugin'])) {
-        if (($_GET['plugin'] !== "") && !preg_match('/[^A-Za-z]/', $_GET['plugin'])) {
-            $output = new WebpageXML($_GET['plugin']);
-        } else {
-            unset($output);
-        }
+    if (!empty($_GET['plugin'])) {
+        $output = new WebpageXML($_GET['plugin']);
     } else {
         $output = new WebpageXML();
     }
+
     // if $output is correct generate output in proper type
-    if (isset($output) && is_object($output)) {
-        if (isset($_GET['json']) || isset($_GET['jsonp'])) {
-            header("Cache-Control: no-cache, must-revalidate\n");
-            $json = $output->getJsonString();
-            if (isset($_GET['jsonp'])) {
-                header("Content-Type: application/javascript\n\n");
-                echo(!preg_match('/[^\w\?]/', $_GET['callback'])?$_GET['callback']:'') . '('.$json.')';
-            } else {
-                header("Content-Type: application/json\n\n");
-                echo $json;
-            }
+    if (isset($_GET['json']) || isset($_GET['jsonp'])) {
+        header("Cache-Control: no-cache, must-revalidate");
+        $json = $output->getJsonString();
+        if (isset($_GET['jsonp'])) {
+            header("Content-Type: application/javascript");
+            echo (!preg_match('/[^\w\?]/', $_GET['callback']) ? $_GET['callback'] : '') . '(' . $json . ')';
         } else {
-            $output->run();
+            header("Content-Type: application/json");
+            echo $json;
         }
+    } else {
+        $output->run();
     }
 }
