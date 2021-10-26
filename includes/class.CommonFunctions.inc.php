7c1de452b80da00a556fe66c559fb2280b7eba2c
namiltd <namiltd@users.noreply.github.com>
2021-10-26 08:59:58 +0200

    code cleaning

--- includes/class.CommonFunctions.inc.php
+++ includes/class.CommonFunctions.inc.php
@@ -646,15 +646,14 @@ class CommonFunctions
                 'XenVMMXenVMM' => 'xen', // Xen hypervisor
                 'ACRNACRNACRN' => 'acrn', // ACRN hypervisor
                 'TCGTCGTCGTCG' => 'qemu', // QEMU
-                'QNXQVMBSQG' => 'qnx', // QNX hypervisor
-                'VBoxVBoxVBox' = > 'oracle' // Oracle VM VirtualBox
+                'QNXQVMBSQG' => 'qnx' // QNX hypervisor
             );
             $shortvendorid = trim(preg_replace('/[\s!\.]/', '', $vendor_data));
             if (($shortvendorid !== "") && isset($vidarray[$shortvendorid])) {
                 return $vidarray[$shortvendorid];
             }
         }
-   
+
         return null;
     }
 
