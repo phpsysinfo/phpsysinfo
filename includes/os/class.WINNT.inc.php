92ef5eedda72aa755b4fe51e905bfb14d9f4e072
namiltd <namiltd@users.noreply.github.com>
2021-11-08 10:23:32 +0100

    code optimization

--- includes/os/class.WINNT.inc.php
+++ includes/os/class.WINNT.inc.php
@@ -369,8 +369,7 @@ class WINNT extends OS
      */
     public static function readReg($reg, $strName, &$strBuffer, $booErrorRep = true, $dword = false, $bits64 = false)
     {
-        $arrBuffer = array();
-        $_hkey = array('HKEY_CLASSES_ROOT'=>0x80000000, 'HKEY_CURRENT_USER'=>0x80000001, 'HKEY_LOCAL_MACHINE'=>0x80000002, 'HKEY_USERS'=>0x80000003, 'HKEY_PERFORMANCE_DATA'=>0x80000004, 'HKEY_PERFORMANCE_TEXT'=>0x80000050, 'HKEY_PERFORMANCE_NLSTEXT'=>0x80000060, 'HKEY_CURRENT_CONFIG'=>0x80000005, 'HKEY_DYN_DATA'=>0x80000006);
+        $strBuffer = '';
 
         if ($reg === false) {
             if (defined('PSI_EMU_HOSTNAME')) {
@@ -410,6 +409,7 @@ class WINNT extends OS
                 }
             }
         } elseif (gettype($reg) === "object") {
+            $_hkey = array('HKEY_CLASSES_ROOT'=>0x80000000, 'HKEY_CURRENT_USER'=>0x80000001, 'HKEY_LOCAL_MACHINE'=>0x80000002, 'HKEY_USERS'=>0x80000003, 'HKEY_PERFORMANCE_DATA'=>0x80000004, 'HKEY_PERFORMANCE_TEXT'=>0x80000050, 'HKEY_PERFORMANCE_NLSTEXT'=>0x80000060, 'HKEY_CURRENT_CONFIG'=>0x80000005, 'HKEY_DYN_DATA'=>0x80000006);
             $first = strpos($strName, "\\");
             $last = strrpos($strName, "\\");
             $hkey = substr($strName, 0, $first);
@@ -450,7 +450,6 @@ class WINNT extends OS
     public static function enumKey($reg, $strName, &$arrBuffer, $booErrorRep = true)
     {
         $arrBuffer = array();
-        $_hkey = array('HKEY_CLASSES_ROOT'=>0x80000000, 'HKEY_CURRENT_USER'=>0x80000001, 'HKEY_LOCAL_MACHINE'=>0x80000002, 'HKEY_USERS'=>0x80000003, 'HKEY_PERFORMANCE_DATA'=>0x80000004, 'HKEY_PERFORMANCE_TEXT'=>0x80000050, 'HKEY_PERFORMANCE_NLSTEXT'=>0x80000060, 'HKEY_CURRENT_CONFIG'=>0x80000005, 'HKEY_DYN_DATA'=>0x80000006);
 
         if ($reg === false) {
             if (defined('PSI_EMU_HOSTNAME')) {
@@ -474,6 +473,7 @@ class WINNT extends OS
                 }
             }
         } elseif (gettype($reg) === "object") {
+            $_hkey = array('HKEY_CLASSES_ROOT'=>0x80000000, 'HKEY_CURRENT_USER'=>0x80000001, 'HKEY_LOCAL_MACHINE'=>0x80000002, 'HKEY_USERS'=>0x80000003, 'HKEY_PERFORMANCE_DATA'=>0x80000004, 'HKEY_PERFORMANCE_TEXT'=>0x80000050, 'HKEY_PERFORMANCE_NLSTEXT'=>0x80000060, 'HKEY_CURRENT_CONFIG'=>0x80000005, 'HKEY_DYN_DATA'=>0x80000006);
             $first = strpos($strName, "\\");
             $hkey = substr($strName, 0, $first);
             if (isset($_hkey[$hkey])) {
