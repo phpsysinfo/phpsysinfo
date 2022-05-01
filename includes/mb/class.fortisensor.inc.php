ef73729a1bcef422206d65f73fc5467315ca8b8e
namiltd <namiltd@users.noreply.github.com>
2022-05-01 11:06:11 +0200

    fortisensor

---
+++ includes/mb/class.fortisensor.inc.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * fortisensor sensor class, getting hardware sensors information from Fortinet devices
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PSI_Sensor
+ * @author    Mieczyslaw Nalewaj <namiltd@users.sourceforge.net>
+ * @copyright 2022 phpSysInfo
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License version 2, or (at your option) any later version
+ * @version   Release: 3.0
+ * @link      http://phpsysinfo.sourceforge.net
+ */
+class FortiSensor extends Sensors
+{
+    /**
+     * content to parse
+     *
+     * @var array
+     */
+    private $_lines = array();
+
+    /**
+     * fill the private array
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $lines = "";
+        if (defined('PSI_EMU_PORT') && CommonFunctions::executeProgram('echo', 'execute sensor list | sshpass -p \''.PSI_EMU_PASSWORD.'\' ssh -T -o \'StrictHostKeyChecking=no\' '.PSI_EMU_USER.'@'.PSI_EMU_HOSTNAME.' -p '.PSI_EMU_PORT, $resulte, false) && ($resulte !== "")
+           && preg_match('/^(.*[\$#]\s*)/', $resulte, $resulto, PREG_OFFSET_CAPTURE)) {
+            $resulti = substr($resulte, strlen($resulto[1][0]));
+            if (preg_match('/(\n.*[\$#])$/', $resulti, $resulto, PREG_OFFSET_CAPTURE)) {
+                $lines = substr($resulti, 0, $resulto[1][1]);
+                if (count(preg_split('/\n/', $lines, -1, PREG_SPLIT_NO_EMPTY)) < 2) {
+                    $lines = "";
+                }
+            }
+        }
+        $this->_lines = preg_split("/\n/", $lines, -1, PREG_SPLIT_NO_EMPTY);
+    }
+
+    /**
+     * get temperature information
+     *
+     * @return void
+     */
+    private function _temperature()
+    {
+        foreach ($this->_lines as $line) {
+            if (preg_match('/^\s*\d+\s(.+)\sTemperature\s+([\d\.]+)\s\S+C$/', $line, $data)) {
+                $dev = new SensorDevice();
+                $dev->setName($data[1]);
+                $dev->setValue($data[2]);
+                $this->mbinfo->setMbTemp($dev);
+            }
+        }
+    }
+
+    /**
+     * get the information
+     *
+     * @see PSI_Interface_Sensor::build()
+     *
+     * @return void
+     */
+    public function build()
+    {
+        $this->_temperature();
+    }
+}
