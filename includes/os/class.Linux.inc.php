e9e917681ec9bd14b36e3e5f655ef2296c9a4f18
namiltd <namiltd@users.noreply.github.com>
2022-12-15 20:16:50 +0100

    cat /etc/os-release

--- includes/os/class.Linux.inc.php
+++ includes/os/class.Linux.inc.php
@@ -2075,10 +2075,9 @@ class Linux extends OS
                     } else {
                         $this->sys->setDistributionIcon($list['solydxk']['Image']);
                     }
-                } elseif (CommonFunctions::fileexists($filename="/etc/os-release")
-                   && CommonFunctions::rfts($filename, $buf, 0, 4096, false)
-                   && (preg_match('/^TAILS_VERSION_ID="?([^"\r\n]+)/m', $buf, $tid_buf)
-                   || preg_match('/^NAME=["\']?([^"\'\r\n]+)/m', $buf, $id_buf))) {
+                } elseif (((defined('PSI_EMU_PORT') && CommonFunctions::executeProgram('cat', '/etc/os-release', $buf, false))
+                    || (!defined('PSI_EMU_PORT') && CommonFunctions::fileexists($filename="/etc/os-release") && CommonFunctions::rfts($filename, $buf, 0, 4096, false)))
+                    && (preg_match('/^TAILS_VERSION_ID="?([^"\r\n]+)/m', $buf, $tid_buf) || preg_match('/^NAME=["\']?([^"\'\r\n]+)/m', $buf, $id_buf))) {
                     if (preg_match('/^TAILS_VERSION_ID="?([^"\r\n]+)/m', $buf, $tid_buf)) {
                         if (preg_match('/^TAILS_PRODUCT_NAME="?([^"\r\n]+)/m', $buf, $desc_buf)) {
                             $this->sys->setDistribution(trim($desc_buf[1])." ".trim($tid_buf[1]));
